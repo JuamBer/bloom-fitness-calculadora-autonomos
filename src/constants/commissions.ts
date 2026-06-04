5072e626357c5137680d6dd540281425bc127b75
Juan Sáez García <juambersaezgarcia@gmail.com>
2026-06-04 20:26:43 +0200

    .

--- src/constants/commissions.ts
+++ src/constants/commissions.ts
@@ -1,8 +1,8 @@
 export const COMMISSIONS = {
   fisio: [
-    { min: 0, max: 1000, professionalRate: 70, companyRate: 30 },
-    { min: 1000, max: 2000, professionalRate: 75, companyRate: 25 },
-    { min: 2000, max: Infinity, professionalRate: 80, companyRate: 20 },
+    { min: 0, max: 1250, professionalRate: 70, companyRate: 30 },
+    { min: 1250, max: 2500, professionalRate: 75, companyRate: 25 },
+    { min: 2500, max: Infinity, professionalRate: 80, companyRate: 20 },
   ],
   nutri: [
     { min: 0, max: 100, professionalRate: 70, companyRate: 30 },
